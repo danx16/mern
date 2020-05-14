0a00ae3b44683954e030093feaad82be0b190ab5
userdan <root.danx20@gmail.com>
2020-05-14 21:38:20 +0800

    Rendering lists of data

--- my-project/src/components/GoalList.js
+++ my-project/src/components/GoalList.js
@@ -4,7 +4,9 @@ const GoalList = (props) => {
     console.log(props.goals);
     return (
           <ul className='list'>
-
+              {props.goals.map((GList) => {
+                  return <li key={GList.id}>{GList.text}</li>
+              })}
           </ul>
     )
 };
